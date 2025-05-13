2eaf6b802bec9abb0e7548fe444f83359f80ea8d
Aiman Hakimi <aiman17hakimi@gmail.com>
2025-05-14 03:04:44 +0800

    idk what i did

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,8 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      supabaseUrl: process.env.SUPABASE_URL,
-      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY,
     },
   },
 })
--- pages/chat.vue
+++ pages/chat.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {ref} from 'vue'
 import { createClient } from '@supabase/supabase-js'
+
 const config = useRuntimeConfig()
 const supabase = createClient(config.public.supabaseUrl, config.public.supabaseAnonKey)
 
